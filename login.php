835c2e0d18af49a0973e55da10e73597c276c4bf
senzxc <devdxd084@gmail.com>
2024-11-20 08:38:43 +0700

    create login.php

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main class="login">
+        <section>
+            <h1>Login</h1>
+
+            <form action="" method="POST">
+                <label for="Username">Username : </label>
+                <input type="text" name="username" id="Username">
+
+                <label for="Password">Password : </label>
+                <input type="text" name="password" id="Password">
+
+                <button type="submit" name="login">Login</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
