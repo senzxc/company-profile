800a364a5cdc98d8424ffb1bca75f3dfc0d6075b
senzxc <devdxd084@gmail.com>
2024-11-22 00:40:29 +0700

    update functions

--- functions.php
+++ functions.php
@@ -36,10 +36,43 @@
                 return false;
         }
 
+        $img_profile = uploadProfile();
+        if (!$img_profile) {
+            return false;
+        }
+
         $password = password_hash($password, PASSWORD_DEFAULT);
 
-        mysqli_query($conn, "INSERT INTO user VALUES ('', '$username', '$password', NOW())");
+        mysqli_query($conn, "INSERT INTO user VALUES ('', '$username', '$password', NOW(), '$img_profile')");
 
         return mysqli_affected_rows($conn);
+    }
+
+    function uploadProfile() {
+        $fileName = $_FILES['img_profile']['name'];
+        $fileSize = $_FILES['img_profile']['size'];
+        $tmpName = $_FILES['img_profile']['tmp_name'];
+
+        $validImgExtension = ['jpg', 'jpeg', 'png'];
+        $imgExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+        if (!in_array($imgExtension, $validImgExtension)) {
+            echo "<script>
+                    alert('File yang diunggah bukan gambar!');
+                </script>";
+                return false;
+        }
+
+        if ($fileSize > 2000000) {
+            echo "<script>
+                    alert('Ukuran file terlalu besar!');
+                </script>";
+                return false;
+        }
+
+        $newFileName = uniqid() . '.' . $imgExtension;
+
+        move_uploaded_file($tmpName, '../assets/uploads/' . $newFileName);
+
+        return $newFileName;
     }
 ?>
