323be1824e9af038926deeb1cc9bd2bc7b5ca71e
senzxc <devdxd084@gmail.com>
2024-11-30 21:39:13 +0700

    update home index

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
                 <div class="wrapper-nav">
                     <h2>Ford</h2>
                     <ul>
-                        <li><a href="#home">Home</a></li>
+                        <li><a href="#home" class="active">Home</a></li>
                         <li><a href="#about">About</a></li>
                         <li><a href="#gallery">Gallery</a></li>
                         <li><a href="#contact">Contact</a></li>
